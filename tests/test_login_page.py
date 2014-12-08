882f736478c57fa09971091666138943b13808b0
Alexander Vasilyev <soulnai@gmail.com>
2014-12-08 17:21:19 +0200

    updated to py.test parameters

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -12,35 +12,57 @@ login = "automation.tc@ukr.net"
 password = "ololobumbum"
 checkin = False
 
+selenium_grid_url = 'http://172.23.62.121:4444/wd/hub'
+chrome_driver = webdriver.Remote(selenium_grid_url, desired_capabilities={'platform': 'ANY', 'browserName': 'chrome', 'version': '', 'javascriptEnabled': True})
+firefox_driver = webdriver.Remote(selenium_grid_url, desired_capabilities={'platform': 'ANY', 'browserName': 'firefox', 'version': '', 'javascriptEnabled': True})
 
-class TestLoginPage(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get("http://www.ukr.net")
-        self.login_page = LoginPage(self.driver)
 
-    def tearDown(self):
-        self.driver.quit()
+#def setUp():
 
-    def test_login_with_wrong_credentials(self):
-        self.login_page.login("name", "pasword")
-        self.assertEqual(self.login_page.error_message().is_displayed(), True, "Should return error-message")
+  #login_page = LoginPage(driver)
 
-    def test_login_with_credentials_in_all_uppercase(self):
-        self.login_page.login(login.upper(), password.upper())
-        self.assertEqual(self.login_page.error_message().is_displayed(), True, "Should return error-message")
+    #def tearDown(self):
+        #self.driver.quit()
+     #   pass
 
-    @pytest.mark.run('first')
-    def test_login(self):
-        self.login_page.login(login, password)
-        global checkin
-        checkin = True
-        self.assertEqual(self.login_page.login_username().is_displayed(), True, "Login should be successfull")
 
-    @pytest.mark.run(after='first')
-    @pytest.mark.skipif("checkin is False")
-    def test_logout(self):
-            self.login_page.login(login, password)
-            self.login_page.logout_button().click()
-            self.assertEqual(self.login_page.login_button().is_displayed(), True, "Logout should be performed correct")
+@pytest.mark.parametrize('driver', [chrome_driver, firefox_driver])
+def test_login_with_wrong_credentials(driver):
+    login_page = LoginPage(driver)
+    driver.get("http://www.ukr.net")
+    login_page.login("name", "pasword")
+    assert login_page.error_message().is_displayed() is True
+    #unittest.TestCase.assertEqual(login_page.error_message().is_displayed(), True, "Should return error-message")
+
+@pytest.mark.parametrize('driver', [chrome_driver, firefox_driver])
+def test_login_with_credentials_in_all_uppercase(driver):
+    login_page = LoginPage(driver)
+    driver.get("http://www.ukr.net")
+    login_page.login(login.upper(), password.upper())
+    assert login_page.error_message().is_displayed() is True
+    #unittest.TestCase.assertEqual(login_page.error_message().is_displayed(), True, "Should return error-message")
+
+
+@pytest.mark.run('first')
+@pytest.mark.parametrize('driver', [chrome_driver, firefox_driver])
+def test_login(driver):
+    login_page = LoginPage(driver)
+    driver.get("http://www.ukr.net")
+    login_page.login(login, password)
+    global checkin
+    checkin = True
+    assert login_page.login_username().is_displayed() is True
+    #unittest.assertEqual(login_page.login_username().is_displayed(), True, "Login should be successfull")
+    login_page.logout_button().click()
+
+@pytest.mark.run(after='first')
+@pytest.mark.skipif("checkin is False")
+@pytest.mark.parametrize('driver', [chrome_driver, firefox_driver])
+def test_logout(driver):
+        login_page = LoginPage(driver)
+        driver.get("http://www.ukr.net")
+        login_page.login(login, password)
+        login_page.logout_button().click()
+        assert login_page.login_button().is_displayed() is True
+        #unittest.assertEqual(login_page.login_button().is_displayed(), True, "Logout should be performed correct")
